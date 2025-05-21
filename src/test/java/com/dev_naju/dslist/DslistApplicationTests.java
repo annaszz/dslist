3b7726a70784f167140699ac75114192ef0640cc
annaszz <annajuuhbs2@gmail.com>
2025-05-21 11:08:11 -0300

    Start Project

---
+++ src/test/java/com/dev_naju/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev_naju.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
